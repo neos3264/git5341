8b812d4e663aea5b61b215d71346fd2fc3045401
neos3264 <bdyfk123@gmail.com>
2025-04-10 20:29:52 +0900

    add prime numbers

--- mynumber.c
+++ mynumber.c
@@ -11,6 +11,18 @@ int main(){
 			printf("%d",i);
 		}
 	}
+	for(int i=2; i<=100; i++){
+		int prime=1;
+		for(int j=2; j<i; j++){
+			if(i%j==0){
+				prime=0;
+				break;
+			}
+		}
+		if(prime){
+			printf("%d",i);
+		}
+	}
 	return 0;
 }
 
